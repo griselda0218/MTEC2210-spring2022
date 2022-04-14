2d0307cce69d18e69244c52e483ed95ccc53136c
griselda <griseldacortez0218@gmail.com>
2022-04-14 16:46:16 -0400

    trigger and tag

---
+++ Assets/scripts/CarMovement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed = 5;
+  
+    void Update()
+    {
+
+        float xValue = speed * Time.deltaTime;
+        transform.Translate(xValue, 0, 0);
+
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Wall")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(collision.gameObject);
+     
+        }
+    }
+
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 5.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xMove = Input.GetAxisRaw("Horizontal");
+        float yMove = Input.GetAxisRaw("Vertical");
+
+        float xMovement = xMove * speed * Time.deltaTime;
+        float yMovement = yMove * speed * Time.deltaTime;
+
+        transform.Translate(xMovement, yMovement, 0);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Triggered");
+        if (other.gameObject.tag == "Goal")
+        {
+            Debug.Log("Area Cleared");
+        }
+     
+    }
+}
